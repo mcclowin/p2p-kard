1d3e69ccf83469ef9102e47457f04a70d20816df
McClowin <mcclowin@agentmail.to>
2026-03-29 19:39:24 +0000

    feat: SEED_DEMO env flag + CORS wildcard for demo deploy

--- django-backend/entrypoint.sh
+++ django-backend/entrypoint.sh
@@ -4,4 +4,9 @@ set -e
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
+# Seed demo data if flag is set
+if [ "$SEED_DEMO" = "true" ]; then
+  python manage.py seed_demo --clear
+fi
+
 exec "$@"
